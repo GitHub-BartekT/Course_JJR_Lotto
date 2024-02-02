60f22bd480d02918a5851359231398d0c731094e
Bartlomiej Tucholski <bartlomiej.tucholski@gmail.com>
2024-02-02 11:43:30 +0100

    Refactor numberReceiverFacade

--- src/main/java/pl/iseebugs/Lotto/domain/numberReceiver/NumberReceiverFacade.java
+++ src/main/java/pl/iseebugs/Lotto/domain/numberReceiver/NumberReceiverFacade.java
@@ -6,22 +6,16 @@ import java.util.Set;
 public class NumberReceiverFacade {
 
     public String inputNumbers(Set<Integer> numbersFromUser){
-        List<Integer> numbersInTheRange = filterAllNumbersInTheRange(numbersFromUser);
-
-        if (hasCorrectNumberOfNumbers(numbersInTheRange)){
-            return "failed";
+        if (filterAllNumbersInTheRange(numbersFromUser)){
+            return "success";
         }
-        return "success";
+        return "failed";
     }
 
-    private List<Integer> filterAllNumbersInTheRange(Set<Integer> numbersFromUser) {
+    private boolean filterAllNumbersInTheRange(Set<Integer> numbersFromUser) {
         return numbersFromUser.stream()
                 .filter(numbers -> numbers <= 99)
                 .filter(numbers -> numbers >= 1)
-                .toList();
-    }
-
-    private boolean hasCorrectNumberOfNumbers(List<Integer> numbersInTheRange) {
-        return numbersInTheRange.size() != 6;
+                .count() == 6;
     }
 }
