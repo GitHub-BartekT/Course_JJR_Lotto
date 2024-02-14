4090ab5a32fd17aebb323bbc159638e1e3ca2125
Bartlomiej Tucholski <bartlomiej.tucholski@gmail.com>
2024-02-14 21:24:40 +0100

    Add Spring and SpringWeb

---
+++ src/main/java/pl/iseebugs/Lotto/domain/LottoApplication.java
@@ -0,0 +1,13 @@
+package pl.iseebugs.Lotto.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LottoApplication.class, args);
+    }
+}
+
