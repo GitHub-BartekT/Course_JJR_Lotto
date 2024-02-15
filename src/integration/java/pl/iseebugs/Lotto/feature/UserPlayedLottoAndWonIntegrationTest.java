2057f2329191c146de2f90843c110ba4f7b1a420
Bartlomiej Tucholski <bartlomiej.tucholski@gmail.com>
2024-02-15 21:26:39 +0100

    Add docker and files to integration tests

---
+++ src/integration/java/pl/iseebugs/Lotto/feature/UserPlayedLottoAndWonIntegrationTest.java
@@ -0,0 +1,12 @@
+package pl.iseebugs.Lotto.feature;
+
+import org.junit.jupiter.api.Test;
+import pl.iseebugs.Lotto.BaseIntegrationTest;
+
+public class UserPlayedLottoAndWonIntegrationTest extends BaseIntegrationTest {
+
+    @Test
+    public void should_user_win_and_system_should_generate_winners(){
+
+    }
+}
