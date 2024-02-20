98f3365853ffb2c336dbc84b233689f28656e4da
Bartlomiej Tucholski <bartlomiej.tucholski@gmail.com>
2024-02-20 12:09:48 +0100

    Add mongoDb

--- src/test/java/pl/iseebugs/Lotto/domain/resultChecker/InMemoryTicketResultRepository.java
+++ src/test/java/pl/iseebugs/Lotto/domain/resultChecker/InMemoryTicketResultRepository.java
@@ -1,5 +1,10 @@
 package pl.iseebugs.Lotto.domain.resultChecker;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.repository.query.FluentQuery;
 import pl.iseebugs.Lotto.domain.resultChecker.dto.TicketResultDto;
 
 import java.time.LocalDateTime;
@@ -7,6 +12,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.function.Function;
 
 class InMemoryTicketResultRepository implements TicketResultRepository {
     Map<String, TicketResult> inMemoryDatabase = new HashMap<>();
@@ -16,6 +22,145 @@ class InMemoryTicketResultRepository implements TicketResultRepository {
                 .stream()
                 .filter(ticketResult -> ticketResult.drawDate().equals(date))
                 .toList();
+    }
+
+    public InMemoryTicketResultRepository() {
+        super();
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+
+    @Override
+    public <S extends TicketResult> S insert(S entity) {
+        return null;
+    }
+
+    @Override
+    public <S extends TicketResult> List<S> insert(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public <S extends TicketResult> List<S> findAll(Example<S> example) {
+        return null;
+    }
+
+    @Override
+    public <S extends TicketResult> List<S> findAll(Example<S> example, Sort sort) {
+        return null;
+    }
+
+    @Override
+    public <S extends TicketResult> List<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public List<TicketResult> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<TicketResult> findAllById(Iterable<String> strings) {
+        return null;
+    }
+
+    @Override
+    public <S extends TicketResult> S save(S entity) {
+        return null;
+    }
+
+    @Override
+    public boolean existsById(String string) {
+        return false;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(String string) {
+
+    }
+
+    @Override
+    public void delete(TicketResult entity) {
+
+    }
+
+    @Override
+    public void deleteAllById(Iterable<? extends String> strings) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends TicketResult> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public List<TicketResult> findAll(Sort sort) {
+        return null;
+    }
+
+    @Override
+    public Page<TicketResult> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends TicketResult> Optional<S> findOne(Example<S> example) {
+        return Optional.empty();
+    }
+
+    @Override
+    public <S extends TicketResult> Page<S> findAll(Example<S> example, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends TicketResult> long count(Example<S> example) {
+        return 0;
+    }
+
+    @Override
+    public <S extends TicketResult> boolean exists(Example<S> example) {
+        return false;
+    }
+
+    @Override
+    public <S extends TicketResult, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
+        return null;
     }
 
     @Override
