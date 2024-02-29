2e7a4d3c308cacf2622a3e983f2674074931f400
Bartlomiej Tucholski <bartlomiej.tucholski@gmail.com>
2024-02-29 22:02:07 +0100

    Add Error tests

--- src/integration/java/pl/iseebugs/Lotto/http/numberGenerator/RandomNumberGeneratorRestTemplateErrorsIntegrationTest.java
+++ src/integration/java/pl/iseebugs/Lotto/http/numberGenerator/RandomNumberGeneratorRestTemplateErrorsIntegrationTest.java
@@ -52,7 +52,7 @@ public class RandomNumberGeneratorRestTemplateErrorsIntegrationTest {
     }
 
     @Test
-    void should_return_null_numbers_when_fault_connection_reset_by_peer(){
+    void should_throw_exception_500_when_fault_connection_reset_by_peer(){
         //given
         wireMockServer.stubFor(WireMock.get("/api/v1.0/random?min=1&max=99&count=25")
                 .willReturn(WireMock.aResponse()
@@ -65,6 +65,115 @@ public class RandomNumberGeneratorRestTemplateErrorsIntegrationTest {
         //then
         assertThat(exception).isInstanceOf(ResponseStatusException.class);
         assertThat(exception.getMessage()).isEqualTo("500 INTERNAL_SERVER_ERROR");
-
     }
+
+
+    @Test
+    void should_throw_exception_500_when_fault_empty_response() {
+        // given
+        wireMockServer.stubFor(WireMock.get("/api/v1.0/random?min=1&max=99&count=25")
+                .willReturn(WireMock.aResponse()
+                        .withStatus(HttpStatus.SC_OK)
+                        .withHeader(CONTENT_TYPE_HEADER_KEY, APPLICATION_JSON_CONTENT_TYPE_VALUE)
+                        .withFault(Fault.EMPTY_RESPONSE)));
+
+        // when
+        Throwable throwable = catchThrowable(() -> randomNumbersGenerable.generateSixRandomNumbers(25, 1, 99));
+
+        // then
+        assertThat(throwable).isInstanceOf(ResponseStatusException.class);
+        assertThat(throwable.getMessage()).isEqualTo("500 INTERNAL_SERVER_ERROR");
+    }
+
+    @Test
+    void should_throw_exception_500_when_fault_malformed_response_chunk() {
+        // given
+        wireMockServer.stubFor(WireMock.get("/api/v1.0/random?min=1&max=99&count=25")
+                .willReturn(WireMock.aResponse()
+                        .withStatus(HttpStatus.SC_OK)
+                        .withHeader(CONTENT_TYPE_HEADER_KEY, APPLICATION_JSON_CONTENT_TYPE_VALUE)
+                        .withFault(Fault.MALFORMED_RESPONSE_CHUNK)));
+        // when
+        Throwable throwable = catchThrowable(() -> randomNumbersGenerable.generateSixRandomNumbers(25, 1, 99));
+
+        // then
+        assertThat(throwable).isInstanceOf(ResponseStatusException.class);
+        assertThat(throwable.getMessage()).isEqualTo("500 INTERNAL_SERVER_ERROR");
+    }
+
+    @Test
+    void should_throw_exception_204_when_status_is_204_no_content() {
+        // given
+        wireMockServer.stubFor(WireMock.get("/api/v1.0/random?min=1&max=99&count=25")
+                .willReturn(WireMock.aResponse()
+                        .withStatus(HttpStatus.SC_NO_CONTENT)
+                        .withHeader(CONTENT_TYPE_HEADER_KEY, APPLICATION_JSON_CONTENT_TYPE_VALUE)
+                        .withBody("""
+                                [1, 2, 3, 4, 5, 6, 82, 82, 83, 83, 86, 57, 10, 81, 53, 93, 50, 54, 31, 88, 15, 43, 79, 32, 43]
+                                          """.trim()
+                        )));
+
+        // when
+        Throwable throwable = catchThrowable(() -> randomNumbersGenerable.generateSixRandomNumbers(25, 1, 99));
+
+        // then
+        assertThat(throwable).isInstanceOf(ResponseStatusException.class);
+        assertThat(throwable.getMessage()).isEqualTo("204 NO_CONTENT");
+    }
+
+    @Test
+    void should_throw_exception_500_when_response_delay_is_5000_ms_and_client_has_1000ms_read_timeout() {
+        // given
+        wireMockServer.stubFor(WireMock.get("/api/v1.0/random?min=1&max=99&count=25")
+                .willReturn(WireMock.aResponse()
+                        .withStatus(HttpStatus.SC_OK)
+                        .withHeader(CONTENT_TYPE_HEADER_KEY, APPLICATION_JSON_CONTENT_TYPE_VALUE)
+                        .withBody("""
+                                [1, 2, 3, 4, 5, 6, 82, 82, 83, 83, 86, 57, 10, 81, 53, 93, 50, 54, 31, 88, 15, 43, 79, 32, 43]
+                                          """.trim()
+                        )
+                        .withFixedDelay(5000)));
+
+        // when
+        Throwable throwable = catchThrowable(() -> randomNumbersGenerable.generateSixRandomNumbers(25, 1, 99));
+
+        // then
+        assertThat(throwable).isInstanceOf(ResponseStatusException.class);
+        assertThat(throwable.getMessage()).isEqualTo("500 INTERNAL_SERVER_ERROR");
+    }
+
+    @Test
+    void should_throw_exception_404_when_http_service_returning_not_found_status() {
+        // given
+        wireMockServer.stubFor(WireMock.get("/api/v1.0/random?min=1&max=99&count=25")
+                .willReturn(WireMock.aResponse()
+                        .withHeader(CONTENT_TYPE_HEADER_KEY, APPLICATION_JSON_CONTENT_TYPE_VALUE)
+                        .withStatus(HttpStatus.SC_NOT_FOUND))
+        );
+
+        // when
+        Throwable throwable = catchThrowable(() -> randomNumbersGenerable.generateSixRandomNumbers(25, 1, 99));
+
+        // then
+        assertThat(throwable).isInstanceOf(ResponseStatusException.class);
+        assertThat(throwable.getMessage()).isEqualTo("404 NOT_FOUND");
+    }
+
+    @Test
+    void should_throw_exception_401_when_http_service_returning_unauthorized_status() {
+        // given
+        wireMockServer.stubFor(WireMock.get("/api/v1.0/random?min=1&max=99&count=25")
+                .willReturn(WireMock.aResponse()
+                        .withHeader(CONTENT_TYPE_HEADER_KEY, APPLICATION_JSON_CONTENT_TYPE_VALUE)
+                        .withStatus(HttpStatus.SC_UNAUTHORIZED))
+        );
+
+        // when
+        Throwable throwable = catchThrowable(() -> randomNumbersGenerable.generateSixRandomNumbers(25, 1, 99));
+
+        // then
+        assertThat(throwable).isInstanceOf(ResponseStatusException.class);
+        assertThat(throwable.getMessage()).isEqualTo("401 UNAUTHORIZED");
+    }
+
 }
--- src/main/java/pl/iseebugs/Lotto/infrastructure/numberGenerator/http/RandomNumberGeneratorRestTemplate.java
+++ src/main/java/pl/iseebugs/Lotto/infrastructure/numberGenerator/http/RandomNumberGeneratorRestTemplate.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
@@ -21,38 +20,59 @@ import java.util.stream.Collectors;
 @Log4j2
 public class RandomNumberGeneratorRestTemplate implements RandomNumbersGenerable {
 
+    public static final String RANDOM_NUMBER_SERVICE_PATH = "/api/v1.0/random";
+
     private final RestTemplate restTemplate;
     private final String uri;
     private final int port;
 
     @Override
     public SixRandomNumbersDto generateSixRandomNumbers(int count, int lowerBound, int upperBound) {
-        String urlForService = getUrlForService("/api/v1.0/random");
+        log.info("Started fetching winning numbers using http client");
         HttpHeaders headers = new HttpHeaders();
         final HttpEntity<HttpHeaders> requestEntity = new HttpEntity<>(headers);
         try {
-            final String url = UriComponentsBuilder.fromHttpUrl(urlForService)
-                    .queryParam("min", lowerBound)
-                    .queryParam("max", upperBound)
-                    .queryParam("count", count)
-                    .toUriString();
-            log.info("Http: {}", url);
-            ResponseEntity<List<Integer>> response = restTemplate.exchange(
-                    url,
-                    HttpMethod.GET,
-                    requestEntity,
-                    new ParameterizedTypeReference<>() {
-                    });
-            Set<Integer> numbers = new HashSet<>(response.getBody());
-            Set<Integer> result = numbers.stream().limit(6).collect(Collectors.toSet());
-            log.info("Winning numbers: {}", result);
+            final ResponseEntity<List<Integer>> response = makeGetRequest(count, lowerBound, upperBound, requestEntity);
+            Set<Integer> sixDistinctNumbers = getSixRandomDistinctNumbers(response);
+            if (sixDistinctNumbers.size() != 6) {
+                log.error("Set is less than: {} Have to request one more time", count);
+                return generateSixRandomNumbers(count, lowerBound, upperBound);
+            }
             return SixRandomNumbersDto.builder()
-                    .numbers(result)
+                    .numbers(sixDistinctNumbers)
                     .build();
         } catch (ResourceAccessException e) {
             log.error("Error while fetching winning numbers using http client: " + e.getMessage());
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+    private ResponseEntity<List<Integer>> makeGetRequest(int count, int lowerBound, int upperBound, HttpEntity<HttpHeaders> requestEntity) {
+        final String url = UriComponentsBuilder.fromHttpUrl(getUrlForService(RANDOM_NUMBER_SERVICE_PATH))
+                .queryParam("min", lowerBound)
+                .queryParam("max", upperBound)
+                .queryParam("count", count)
+                .toUriString();
+        ResponseEntity<List<Integer>> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                requestEntity,
+                new ParameterizedTypeReference<>() {
+                });
+        return response;
+    }
+
+    private Set<Integer> getSixRandomDistinctNumbers(ResponseEntity<List<Integer>> response) {
+        List<Integer> numbers = response.getBody();
+        if (numbers == null) {
+            log.error("Response Body was null");
+            throw new ResponseStatusException(HttpStatus.NO_CONTENT);
+        }
+        log.info("Success Response Body Returned: " + response);
+        Set<Integer> distinctNumbers = new HashSet<>(numbers);
+        return distinctNumbers.stream()
+                .limit(6)
+                .collect(Collectors.toSet());
     }
 
     private String getUrlForService(String service) {
